83d21381802a494ce023f6672f544304ab0fa006
Helga Ganz <helgaelisa.ganz@gmail.com>
2017-04-03 14:12:28 +0200

    new relic data displayed in widget

--- test/new_relic_test.exs
+++ test/new_relic_test.exs
@@ -2,9 +2,12 @@ defmodule NewRelicTest do
   use ExUnit.Case
 
   import Kitto.Jobs.NewRelic
+  @api_key [{"X-Api-Key", "***REMOVED***"}]
 
   test ": new relic is correctly pulled" do
-    {status, body}  = fetch()
+    {status, body}  = new_relic_url()
+                          |> HTTPoison.get(@api_key)
+                          |> parse_response
     assert status == :ok
   end
   
