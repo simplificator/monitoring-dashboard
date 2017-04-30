098c5a0f0be50be5cd95fb222f0b72ecc9179965
Marion Schleifer <marion.schleifer@simplificator.com>
2017-04-30 15:39:42 +0200

    fix naming problem

--- apps/http_server/lib/http_server/controllers/main.ex
+++ apps/http_server/lib/http_server/controllers/main.ex
@@ -2,7 +2,7 @@ defmodule HttpServer.Controllers.Main do
   use Sugar.Controller
 
   def index(conn, []) do
-    IO.puts Kitto.Jobs.NewRelic.new_relic_url()
+    IO.puts Kitto.Api.NewRelic.new_relic_url()
     raw conn |> resp(200, "Hello world")
   end
 end
