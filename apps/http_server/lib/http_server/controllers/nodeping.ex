4a5e6d6c7c16072d674480a655902b8b4c1249d7
Helga Ganz <helgaelisa.ganz@gmail.com>
2017-05-08 09:04:29 +0200

    revert last commit

--- apps/http_server/lib/http_server/controllers/nodeping.ex
+++ apps/http_server/lib/http_server/controllers/nodeping.ex
@@ -2,8 +2,10 @@ defmodule HttpServer.Controllers.Nodeping do
   use Sugar.Controller
 
   def get(conn, []) do
+    IO.puts "org result: "<> Kernel.inspect(conn.params)
     nodeping_list = filter_list(conn.params)
-    Kitto.Jobs.Nodeping.receive_push_and_display_list(nodeping_list)
+    IO.puts "controller list: " <> Kernel.inspect(nodeping_list)
+    Kitto.Api.Nodeping.receive_push_and_display_list(nodeping_list)
     raw conn |> resp(200, "Nodeping")
   end
 
@@ -13,6 +15,13 @@ defmodule HttpServer.Controllers.Nodeping do
       [ %{label: head["label"], value: head["event"]} | filter_list(tail) ]
     else
       filter_list(tail)
+    end
+  end
+  def filter_list(head) do
+    if head["event"] in ["down"] do
+      [ %{label: head["label"], value: head["event"]} ]
+    else
+      []
     end
   end
 end
