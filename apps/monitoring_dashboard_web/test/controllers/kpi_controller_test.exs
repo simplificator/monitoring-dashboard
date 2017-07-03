5b358f390a26cd31c6dd6a8d910c074a8b604d2b
Helga Ganz <helgaelisa.ganz@gmail.com>
2017-07-03 08:09:33 +0200

    bugfix test

--- apps/monitoring_dashboard_web/test/controllers/kpi_controller_test.exs
+++ apps/monitoring_dashboard_web/test/controllers/kpi_controller_test.exs
@@ -2,18 +2,18 @@ defmodule Controllers.KpiControllerTest do
   use ExUnit.Case
 
   test "get kpi data" do
-    params = %{
-    "api_key": "bpsnBgbCVeM4",
-    "data": %{
-    "title": "Billable hours (this week)",
-    "series": [%{
-    "data": [55, 45, 0, 0, 0]
-    }],
-    "x_axis": %{
-    "labels": ["Mon", "Tue", "Wed", "Thu", "Fri"]
-    }
-    }
-    }
+    params = Poison.Parser.parse!("{
+              \"api_key\": \"bpsnBgbCVeM4\",
+              \"data\": {
+              \"title\": \"Billable hours (this week)\",
+              \"series\": [{
+              \"data\": [55, 45, 0, 0, 0]
+              }],
+              \"x_axis\": {
+              \"labels\": [\"Mon\", \"Tue\", \"Wed\", \"Thu\", \"Fri\"]
+              }
+              }
+              }")
     result = MonitoringDashboard.Web.KpiController.get_data(params)
     assert result == [%{x: 0, y: 55}, %{x: 1, y: 45}, %{x: 2, y: 0}, %{x: 3, y: 0}, %{x: 4, y: 0}]
   end
